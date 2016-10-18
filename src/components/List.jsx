02454df8f6c13fe1af50d135e40edd4f6af2f6ab
Krs Joseph <krspjoseph@gmail.com>
2016-10-18 11:10:29 -0400

    add list and list item components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import ListItem from './ListItem'
 
-
 export default class List extends Component{
   render(){
 	  let courses = this.props.courses.map(data => <ListItem key={data.image} {...data} />);
 	  return(
-  		<ol>
-			  {courses}
-		  </ol>
+	  	<div>
+		    <h1>Udacity Course List</h1>
+	      <ol>
+				  {courses}
+			  </ol>
+		  </div>
 	  )
   }
 }
